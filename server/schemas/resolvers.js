62a74cffdbe728f0663850c692762ffaa50d0e23
Thomas James <tomsterlingjames@outlook.com.au>
2022-05-19 08:52:22 +1000

    linked models in index.js and added args toresolver context

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -24,7 +24,7 @@ const resolvers = {
     product: async (parent, { _id }) => {
         return await Product.findById(_id).populate('category');
       },
-      user: async (parent, context) => {
+      user: async (parent, args, context) => {
         if (context.user) {
           const user = await User.findById(context.user._id).populate({
             path: 'orders.products',
