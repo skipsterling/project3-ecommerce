a13a2f7b198ebc6d367d0dcf4e9399d4ddf75d1e
Thomas James <tomsterlingjames@outlook.com.au>
2022-05-19 17:40:46 +1000

    mutation info added to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -37,6 +37,13 @@ const typeDefs = gql`
     order(_id: ID!) Order
     checkout(products: [ID]!): Checkout
   }
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(firstName: String!, lastName: String!, email: String!, password: String!): User
+    updateProduct( _id: ID!, quantity: Int!): Product
+    login(email: String!, password: Sting!): Auth
+  }
 `;
 
 module.exports = typeDefs;
