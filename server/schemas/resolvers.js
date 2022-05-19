82171fe55dc99f65e3b4fcfc39c5c6d703210658
Thomas James <tomsterlingjames@outlook.com.au>
2022-05-19 14:59:46 +1000

    adding stripe dependany aswell as updating resolvers file with a checkout

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
+const stripe = ('stripe')('sk_test_51L10skFcX4c4Cq5QpUH2kP38nQcH4LGjFFRa9SMRtPlI9fw9XPyXnnblz3kuh8UQ97Khc9uMMqFehuch180hX8Rh00tVhVtSnY');
 
 const resolvers = {
     Query: {
@@ -51,4 +52,38 @@ const resolvers = {
         throw new AuthenticationError('Not logged in');
       };
 
+      // adding stripe checkout.
+      checkout: async (parent, args, context) => {
+        const url = new URL(context.headers.referer).origin;
+        const order = new Order({ products: args.products });
+        const line_items = [];
+        const { products } = await order.populate('products').execPopulate();
+        for (let i =0; i < products.length; i++) {
+          const product = await stripe.products.create({
+            name: products[i].name,
+            description: products[i].description,
+            images: [`${url}/images${products[i].image}`]
+          });
+
+          const price = await stripe.prices.create({
+            product: product.id,
+            unit_amount:products[i].price * 100,
+            currency: 'aud',
+          });
+          // using line_items to send it to stripe.
+          line_items.push({
+            price: price.id,
+            quantity: 1
+          });
+        }
+        const session = await stripe.checkout.sessions.create({
+          payment_method_types: ['card'],
+          line_items,
+          mode: 'payment',
+          success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${url}/`
+        });
+        return { session: session.id };
+      }
+    },
       module.exports = resolvers;
