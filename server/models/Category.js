3fbbeba0aea8c8d425b8ee2bd4bb50ce5bd369ca
Thomas James <tomsterlingjames@outlook.com.au>
2022-05-19 10:02:40 +1000

    category model done

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,2 +1,12 @@
- const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+const Category = mongoose.model('Category', categorySchema);
+module.exports = Category;
