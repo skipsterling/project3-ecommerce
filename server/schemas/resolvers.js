6884db375be72f28c245e038f64a0dc0f9746b36
Thomas James <tomsterlingjames@outlook.com.au>
2022-05-20 09:49:59 +1000

    forgot to add a const for sign token and also forgot to add requrie to my stripe const.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
-const stripe = ('stripe')('sk_test_51L10skFcX4c4Cq5QpUH2kP38nQcH4LGjFFRa9SMRtPlI9fw9XPyXnnblz3kuh8UQ97Khc9uMMqFehuch180hX8Rh00tVhVtSnY');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_51L10skFcX4c4Cq5QpUH2kP38nQcH4LGjFFRa9SMRtPlI9fw9XPyXnnblz3kuh8UQ97Khc9uMMqFehuch180hX8Rh00tVhVtSnY');
 
 const resolvers = {
   Query: {
@@ -37,7 +38,7 @@ const resolvers = {
         return user;
       }
 
-      throw new AuthenticationError('Not logged in');
+      throw new AuthenticationError('Sorry, you must be logged in to continue.');
     },
     order: async (parent, { _id }, context) => {
       if (context.user) {
@@ -49,86 +50,87 @@ const resolvers = {
         return user.orders.id(_id);
       }
 
-      throw new AuthenticationError('Not logged in');
+      throw new AuthenticationError('Sorry, you must be logged in to continue.');
     },
-      // adding stripe checkout.
-      checkout: async (parent, args, context) => {
-        const url = new URL(context.headers.referer).origin;
-        const order = new Order({ products: args.products });
-        const line_items = [];
-        const { products } = await order.populate('products').execPopulate();
-        for (let i =0; i < products.length; i++) {
-          const product = await stripe.products.create({
-            name: products[i].name,
-            description: products[i].description,
-            images: [`${url}/images${products[i].image}`]
-          });
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ products: args.products });
+      const line_items = [];
 
-          const price = await stripe.prices.create({
-            product: product.id,
-            unit_amount:products[i].price * 100,
-            currency: 'aud',
-          });
-          // using line_items to send it to stripe.
-          line_items.push({
-            price: price.id,
-            quantity: 1
-          });
-        }
-        const session = await stripe.checkout.sessions.create({
-          payment_method_types: ['card'],
-          line_items,
-          mode: 'payment',
-          success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-          cancel_url: `${url}/`
+      const { products } = await order.populate('products').execPopulate();
+
+      for (let i = 0; i < products.length; i++) {
+        const product = await stripe.products.create({
+          name: products[i].name,
+          description: products[i].description,
+          images: [`${url}/images/${products[i].image}`]
         });
-        return { session: session.id };
+
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: 'usd',
+        });
+
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
       }
-    },
 
-    // Adding mutations to write data back to the database.
-    mutation: {
-      addUser: async (parent, args) => {
-        const user = await User.create(args.user);
-        const token = signToken(user);
-        return { token, user };
-      },
-      addOrder: async (parent, { products}, context) => {
-        console.log('context');
-        if (context.user) {
-          const order = new Order({ products });
-          await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
-          return order;
-        }
-        throw new AuthenticationErro('Sorry, you must be logged in to continue.');
-      },
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
+      });
 
-      updateUser: async (parent, args, context) => {
-        if (context.user) {
-          return await User.findByIdAndUpdate(context.user._id, args, { new: true });
-          }
-          throw new AuthenticationErro('Sorry, you must be logged in to continue.');
-        },
-        
-        updateProduct: async (parent, { _id, quantity }) => {
-          const decrement = Math.abs(quantity) * -1;
-          return await Product.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true});
-        },
-
-        login: async (parent, { email, password }) => {
-          const user = await User.findOne({ email });
-          if (!user) {
-            throw new AuthenticationError('This email or password is incorrect');
-          }
-          const correctPW = await user.isIncorrectPassword(password);
-          if (!correctPW) {
-        throw new AuthenticationError('Sorry, that password is incorrect. Please try again.');
-          }
+      return { session: session.id };
+    }
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
       const token = signToken(user);
       return { token, user };
-        }
+    },
+    addOrder: async (parent, { products }, context) => {
+      console.log(context);
+      if (context.user) {
+        const order = new Order({ products });
+        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
+        return order;
       }
-    };
-      module.exports = resolvers;
+      throw new AuthenticationError('Sorry, you must be logged in to continue.');
+    },
+
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+      }
+      throw new AuthenticationError('Sorry, you must be logged in to continue.');
+    },
+
+    updateProduct: async (parent, { _id, quantity }) => {
+      const decrement = Math.abs(quantity) * -1;
+      return await Product.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true });
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('This email or password is incorrect');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Sorry, that password is incorrect. Please try again.');
+      }
+      const token = signToken(user);
+      return { token, user };
+    }
+  }
+};
+module.exports = resolvers;
 
       
