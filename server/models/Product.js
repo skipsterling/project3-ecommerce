fbace19aa80604ce709618cb32364885aa141be0
Thomas James <tomsterlingjames@outlook.com.au>
2022-05-19 10:46:34 +1000

    product model added

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,2 +1,34 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    Description: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0.99
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    }
+});
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
