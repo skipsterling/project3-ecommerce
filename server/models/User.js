ef9e5e71f56a32de4d434f7085e5caa312e24b5b
Thomas James <tomsterlingjames@outlook.com.au>
2022-05-19 11:15:40 +1000

    user model started, still need to make it safe with bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,2 +1,29 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const Order = require('./Order');
+
+const userSchema = new Schema({
+    firstName: { 
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: { 
+        type: String,
+        requried: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        requried: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        requrired: true,
+        minLength: 8
+    },
+    orders: [Order.schema]
+});
+
+const User = mongoose.model('User', userSchema);
