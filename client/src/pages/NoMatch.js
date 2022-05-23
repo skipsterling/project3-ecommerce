e3717eb6771b7b6162e59a8500d320bd77c78e07
Thomas James <tomsterlingjames@outlook.com.au>
2022-05-23 20:20:35 +1000

    404 page added

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,20 @@
+// Creating a 404 page.
+import React from "react";
+import Jumbotron from "../components/Jumbotron";
+
+const NoMatch = () => {
+  return (
+    <div>
+      <Jumbotron>
+        <h1>404 Page Not Found</h1>
+        <h1>
+          <span role="img" aria-label="smh Emoji">
+          🤦‍♂️ 
+          </span>
+        </h1>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default NoMatch;
