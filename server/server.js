a2e7af968d704208d92a6c3a99db46f1e3eb1640
Thomas James <tomsterlingjames@outlook.com.au>
2022-05-28 07:15:05 +1000

    preparing to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -15,9 +15,11 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
